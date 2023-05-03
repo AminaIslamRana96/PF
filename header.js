1e1167983da0e3c2fe64d2e672fdbeaefd0cbe44
AminaIslamRana96 <amina.islam19960@gmail.com>
2023-05-04 01:20:40 +0500

    changes

--- header.js
+++ header.js
@@ -37,6 +37,10 @@ document.getElementById("header").innerHTML =
          <li class="nav-item">
            <a class="nav-link" href="contact.html">Contact</a>
          </li>
+         <li class="nav-item">
+         
+           
+         </li>
        </ul>
        <form class="d-flex">
          <input
@@ -48,6 +52,27 @@ document.getElementById("header").innerHTML =
          <button class="btn btn-color btn-outline-dark" type="submit">
            Search
          </button>
+
+         
+         <div class="cart-modal-overlay">
+    <div class="cart-modal">
+      <i id="close-btn" class="fas fa-times"></i>
+        <h1 class="cart-is-empty">Cart is empty</h1>
+      
+        <div class="product-rows">
+        </div>
+        <div class="total">
+          <h1 class="cart-total">TOTAL</h1>
+            <span class="total-price">$0</span>
+              <button class="purchase-btn">PURCHASE</button>
+        </div>
+      </div>
+</div>
+         <div class="cart-btn">
+    <i id="cart" class="fas fa-shopping-cart"></i>
+      <span class ="cart-quantity">0</span>
+  </div>
+           
        </form>
      </div>
    </div>
