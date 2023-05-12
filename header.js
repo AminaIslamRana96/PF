28e904cadc20d2eea3c2004a000091204bed8813
AminaIslamRana96 <amina.islam19960@gmail.com>
2023-05-12 11:17:44 +0500

    menu

--- header.js
+++ header.js
@@ -5,22 +5,11 @@ document.getElementById("header").innerHTML =
 
 <div class="container-fluid navigation-bg">
    <div class="container">
-   <nav class="navbar navbar-expand-lg navigation-bg">
+   <nav class="navbar navbar-default navbar-fixed-top navbar-expand-lg navigation-bg">
    <div class="container-fluid">
      <img src="images/logo.webp" alt="" srcset="" />
-     <button
-       class="navbar-toggler"
-       type="button"
-       data-bs-toggle="collapse"
-       data-bs-target="#navbarToggleExternalContent" 
-       aria-controls="navbarToggleExternalContent"
-       aria-expanded="false"
-       aria-label="Toggle navigation"
-     >
-       <span class="navbar-toggler-icon"></span>
-     </button>
-     <div class="collapse navbar-collapse ml-4" id="navbarScroll">
-     
+    
+     <div class="collapse navbar-collapse ml-4" id="navbarToggleExternalContent">    
        <ul
          class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
          style="--bs-scroll-height: 100px"
@@ -31,7 +20,6 @@ document.getElementById("header").innerHTML =
            >
          </li>
          <li class="nav-item dropdown">
-         
            <a class="nav-link" href="product.html">Products</a>
            <div class="dropdown-content">
            <a href="#">Eyes</a>
@@ -45,8 +33,8 @@ document.getElementById("header").innerHTML =
          <li class="nav-item">
            <a class="nav-link" href="contact.html">Contact</a>
          </li>
-    
        </ul>
+       </div>
        <form class="d-flex align-self">
          <input
            class="form-control me-2"
@@ -57,29 +45,39 @@ document.getElementById("header").innerHTML =
          <button class="btn btn-color btn-outline-dark " type="submit">
            Search
          </button>
-
-         
-         <div class="cart-modal-overlay">
-            <div class="cart-modal">
-              <i id="close-btn" class="fas fa-times"></i>
-                    <h1 class="cart-is-empty">Cart is empty</h1>
-      
-              <div class="product-rows"></div>
-              <div class="total">
-                 <h1 class="cart-total">TOTAL</h1>
-                <span class="total-price">$0</span>
-                <button class="purchase-btn">PURCHASE</button>
-              </div>
-            </div>
-          </div>
-          <div class="cart-btn">
-          <i id="cart" class="fas fa-shopping-cart"></i>
-            
-            <span class ="cart-quantity">0</span>
-          </div>
-           
+        
        </form>
+       <div class="cart-modal-overlay">
+       <div class="cart-modal">
+         <i id="close-btn" class="fas fa-times"></i>
+               <h1 class="cart-is-empty">Cart is empty</h1>
+ 
+         <div class="product-rows"></div>
+         <div class="total">
+            <h1 class="cart-total">TOTAL</h1>
+           <span class="total-price">$0</span>
+           <button class="purchase-btn">PURCHASE</button>
+         </div>
+       </div>
      </div>
+     <div class="cart-btn">
+     <i id="cart" class="fas fa-shopping-cart"></i>
+       
+       <span class ="cart-quantity">0</span>
+     </div>
+      
+       <button
+       class="navbar-toggler navbar-light pull-right"
+       type="button"
+       data-bs-toggle="collapse"
+       data-bs-target="#navbarToggleExternalContent" 
+       aria-controls="navbarToggleExternalContent"
+       aria-expanded="false"
+       aria-label="Toggle navigation"
+     >
+       <span class="navbar-toggler-icon"></span>
+     </button>
+     
    </div>
   </nav>
        </div>
